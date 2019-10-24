36778805ab8a1216c3514d6a6aa09b0b4ab02138
Nicolas Roos <nicolas.k.roos@gmail.com>
2019-10-24 11:40:21 +0200

    feat(middleware): added subdomain functionality for canonical domains

--- canonical_domain/middleware.py
+++ canonical_domain/middleware.py
@@ -15,6 +15,9 @@ class CanonicalDomainMiddleware(SecurityMiddleware):
         self.canonical_domain_secure = getattr(
             settings, "CANONICAL_DOMAIN_SECURE", False
         )
+        self.allowed_subdomains = getattr(
+            settings, "CANONICAL_DOMAIN_ALLOWED_SUBDOMAINS", []
+        )
 
     def process_request(self, request):
         # Only redirect safe methods according to the RFC:
@@ -26,17 +29,27 @@ class CanonicalDomainMiddleware(SecurityMiddleware):
             "TRACE",
         }:
             return
-
-        matches = request.get_host() == self.canonical_domain
+        host = request.get_host()
+        matches = host == self.canonical_domain or any(
+            map(
+                lambda x: ("%s.%s" % (x, self.canonical_domain) == host),
+                self.allowed_subdomains,
+            )
+        )
         if matches and self.canonical_domain_secure and request.is_secure():
             return
         elif matches and not self.canonical_domain_secure:
             return
 
+        canonical_subdomain = next(
+            (sub for sub in self.allowed_subdomains if sub in host), None
+        )
+
         return HttpResponsePermanentRedirect(
-            "http%s://%s%s"
+            "http%s://%s%s%s"
             % (
                 "s" if (self.canonical_domain_secure or request.is_secure()) else "",
+                "%s." % canonical_subdomain if canonical_subdomain is not None else "",
                 self.canonical_domain,
                 request.get_full_path(),
             )
